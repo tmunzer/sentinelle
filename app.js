382fbd5ae7860565efdb5a0ad9c14446b819612e
tmunzer <tmunzer@ah-lab.fr>
2015-09-03 11:33:03 +0200

    150903

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var sentinelle = require("./sentinelle");
+
+var server_port = 80;
 
 app.use(express.static(__dirname+'/static'));
 app.get('/', function(req, res) {
     res.render("sentinelle.ejs");
-    })
-    .get("/ap", function(req, res) {
-        res.setHeader("Content-Type", "text/plain");
-        res.end("not yet...")
-    })
-    .get("/sta", function(req, res) {
-        res.setHeader("Content-Type", "text/plain");
-        res.end("not yet...")
-    })
-    .get("/config", function(req, res) {
-        res.setHeader("Content-Type", "text/plain");
-        res.end("not yet...")
-    })
-    .use(function(req, res, next){
-        res.setHeader('Content-Type', 'text/plain');
-        res.send(404, 'Page introuvable !');
     });
-app.listen(80);
+
+
+
+io.sockets.on('connection', function(socket) {
+    console.log('new connection');
+});
+
+sentinelle.capture();
+server.listen(server_port, function () {
+    console.log("Server listening on port " + server_port + ".");
+});
+
+module.exports.io=io;
--- sentinelle.js
+++ sentinelle.js
@@ -1,54 +1,85 @@
+var app = require("./app.js");
 var pcap = require("./pcap"),
-pcap_session = pcap.createSession("wlan0mon");
+	pcap_session = pcap.createSession("wlan0mon");
 var sys = require("sys");
+var EventEmitter = require('events').EventEmitter;
 
-pcap_session.on("packet", function(raw_packet){
-    var packet = pcap.decode.packet(raw_packet);
-    //control frames
-    if (packet.payload.ieee802_11Frame.type == 0){
-	//beacons
-	if (packet.payload.ieee802_11Frame.subType == 8){
-	    access_point(packet.payload.ieee802_11Frame);
-	}
-    }
-    else if (packet.payload.ieee802_11Frame.type == 2){
+
+sentinelle_capture = function(){
+	pcap_session.on("packet", function(raw_packet){
+		var packet = pcap.decode.packet(raw_packet);
+		//control frames
+		if (packet.payload.ieee802_11Frame.type == 0){
+			//beacons
+			if (packet.payload.ieee802_11Frame.subType == 8){
+				access_point(packet.payload.ieee802_11Frame);
+			}
+		}
+		else if (packet.payload.ieee802_11Frame.type == 2){
 //    console.log(sys.inspect(packet));
-	console.log(sys.inspect(packet.payload.ieee802_11Frame.llc));
-    }
+			console.log(sys.inspect(packet.payload.ieee802_11Frame.llc));
+		}
 
-});
+	});
+};
 
 var accessPointsList = [];
 
+get_access_points = function(){
+	return accessPointsList;
+};
+
 function AccessPoint (BSSID, beacon){
-    this.BSSID = BSSID;
+	this.BSSID = BSSID;
 	this.beacon = beacon;
-    this.SSID = "";
-    this.rates = "";
-    this.channel = "";
-    this.extended_rates = "";
-    this.country = "";
+	this.SSID = "";
+	this.rates = "";
+	this.channel = "";
+	this.extended_rates = "";
+	this.country = "";
 }
 
 AccessPoint.prototype.set_SSID = function(SSID){
-    this.SSID = SSID;
+	this.SSID = SSID;
 };
 AccessPoint.prototype.set_rates = function(rates){
-    this.rates = rates;
+	this.rates = rates;
 };
 AccessPoint.prototype.set_channel = function(channel){
-    this.channel = channel;
+	this.channel = channel;
 };
 AccessPoint.prototype.set_extended_rates = function(erates){
-    this.extended_rates = erates;
+	this.extended_rates = erates;
 };
 AccessPoint.prototype.set_country = function(country){
-    this.country = country;
+	this.country = country;
+};
+
+AccessPoint.prototype.get_BSSID = function(){
+	return this.BSSID;
+};
+AccessPoint.prototype.get_SSID = function(){
+	return this.SSID;
+};
+AccessPoint.prototype.get_beacon = function(){
+	return this.beacon;
+};
+AccessPoint.prototype.get_rates = function(){
+	return this.rates;
+};
+AccessPoint.prototype.get_channel = function(){
+	return this.channel;
+};
+AccessPoint.prototype.get_extended_rates = function(){
+	return this.extended_rates;
+};
+AccessPoint.prototype.get_country = function(){
+	return this.country;
 };
 
 access_point = function(payload){
-    var BSSID = payload.shost.addr;
-    if (!accessPointsList.hasOwnProperty(BSSID)){
+	var BSSID = payload.shost.addr;
+	if (!accessPointsList.hasOwnProperty(BSSID)){
 		var accessPoint = new AccessPoint(BSSID, payload.beacon);
 		for (var tag in payload.beacon.tags) {
 			if (payload.beacon.tags[tag].hasOwnProperty("type")) {
@@ -75,7 +106,12 @@ access_point = function(payload){
 				}
 			}
 		}
-	console.log(accessPoint);
-	accessPointsList[BSSID]=accessPoint;
-    }
+		console.log(accessPoint);
+		app.io.sockets.emit('test', 'accessPoint');
+		accessPointsList[BSSID]=accessPoint;
+	}
 };
+
+
+exports.capture = sentinelle_capture;
+exports.get_access_points = get_access_points;
