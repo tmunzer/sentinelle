6d39fcbbdeac1451d139ddb4d887832a24c15292
tmunzer <tmunzer@ah-lab.fr>
2015-09-05 17:32:29 +0200

    bug correction

--- decode/ieee802.11/radio_frame.js
+++ decode/ieee802.11/radio_frame.js
@@ -14,7 +14,7 @@ function RadioFrameFlags(emitter) {
 }
 
 //flags should be a uint8LE
-RadioFrameFlags.prototype.decode = function decode (flags) {
+RadioFrameFlags.prototype.decode = function decode(flags) {
     this.raw = flags;
     this.toDS = Boolean((flags >> 0) & 0x0001);
     this.fromDS = Boolean((flags >> 1) & 0x0001);
@@ -35,9 +35,11 @@ function RadioFrame(emitter) {
     this.subType = undefined;
     this.flags = undefined;
     this.duration = undefined;
+    this.receiver_address = undefined;
+    this.destination_address = undefined;
+    this.transmitter_address = undefined;
+    this.source_address = undefined;
     this.bssid = undefined;
-    this.shost = undefined;
-    this.dhost = undefined;
     this.fragSeq = undefined;
     this.beacon = undefined;
     this.llc = undefined;
@@ -46,38 +48,77 @@ function RadioFrame(emitter) {
 RadioFrame.prototype.decode = function (raw_packet, offset) {
     //Check packet length once per decode to avoid having to check it
     //everytime one parses a single value out of th buffer.
-    if(raw_packet.length-offset > 24) {
+    if (raw_packet.length - offset > 24) {
 //        throw "Not enough of packet left to be a RadioFrame";
 //    }
 
-    this.frameControl = raw_packet.readUInt16LE(offset, true); offset += 2;
-    this.version = this.frameControl & 0x0003;
-    this.type = (this.frameControl >> 2) & 0x0003;
-    this.subType = (this.frameControl >> 4) & 0x000f;
-    this.flags = new RadioFrameFlags().decode((this.frameControl >> 8) & 0xff);
-    this.duration = raw_packet.readUInt16BE(offset, true); offset += 2;
-    this.bssid = new EthernetAddr(raw_packet, offset); offset += 6;
-    this.shost = new EthernetAddr(raw_packet, offset); offset += 6;
-    this.dhost = new EthernetAddr(raw_packet, offset); offset += 6;
-    this.fragSeq = raw_packet.readUInt16BE(offset, true); offset += 2;
-
-    if (this.type === 0) {
-        switch(this.subType) {
-        case 4:
-            this.probe = new RadioProbeFrame().decode(raw_packet, offset);
-            break;
-        case 8: // Beacon
-            this.beacon = new RadioBeaconFrame().decode(raw_packet, offset);
-            break;
+        this.frameControl = raw_packet.readUInt16LE(offset, true);
+        offset += 2;
+        this.version = this.frameControl & 0x0003;
+        this.type = (this.frameControl >> 2) & 0x0003;
+        this.subType = (this.frameControl >> 4) & 0x000f;
+        this.flags = new RadioFrameFlags().decode((this.frameControl >> 8) & 0xff);
+        this.duration = raw_packet.readUInt16BE(offset, true);
+        offset += 2;
+        if (!this.flags.fromDS && !this.flags.toDS) {
+            this.receiver_address = new EthernetAddr(raw_packet, offset);
+            this.destination_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.transmitter_address = new EthernetAddr(raw_packet, offset);
+            this.source_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.bssid = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+        } else if (this.flags.fromDS && !this.flags.toDS) {
+            this.receiver_address = new EthernetAddr(raw_packet, offset);
+            this.destination_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.transmitter_address = new EthernetAddr(raw_packet, offset);
+            this.bssid = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.source_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+        } else if (!this.flags.fromDS && this.flags.toDS) {
+            this.receiver_address = new EthernetAddr(raw_packet, offset);
+            this.bssid = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.transmitter_address = new EthernetAddr(raw_packet, offset);
+            this.source_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.destination_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+        } else if (this.flags.fromDS && this.flags.toDS) {
+            this.receiver_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.transmitter_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.destination_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
+            this.source_address = new EthernetAddr(raw_packet, offset);
+            offset += 6;
         }
+        this.fragSeq = raw_packet.readUInt16BE(offset, true);
+        offset += 2;
 
-    } else if (this.type === 2) { // Data Frame
-        if (!this.flags.encrypted && this.subType !== 36) { // subType 36 is a null data frame
-            this.llc = new LogicalLinkControl(this.emitter).decode(raw_packet, offset);
+        if (this.type === 0) {
+            switch (this.subType) {
+                case 4:
+                    this.probe = new RadioProbeFrame().decode(raw_packet, offset);
+                    break;
+                case 8: // Beacon
+                    this.beacon = new RadioBeaconFrame().decode(raw_packet, offset);
+                    break;
+            }
+
+        } else if (this.type === 2) { // Data Frame
+            if (!this.flags.encrypted && this.subType !== 36) { // subType 36 is a null data frame
+                this.llc = new LogicalLinkControl(this.emitter).decode(raw_packet, offset);
+            }
         }
     }
+    if (this.emitter) {
+        this.emitter.emit("radio-frame", this);
     }
-    if(this.emitter) { this.emitter.emit("radio-frame", this); }
     return this;
 };
 
